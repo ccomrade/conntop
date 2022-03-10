780637c7bd9615598cfdfab26692eb6aa327297f
ccomrade <d.hryzbil@gmail.com>
2022-03-11 00:29:32 +0100

    Remove Reactor and TCP sockets

--- Code/App/App.cpp
+++ Code/App/App.cpp
@@ -2,13 +2,9 @@
 
 void App::Run()
 {
-	m_reactor.Init();
-
 	Log::Error("Error");
 	Log::Warning("Warning");
 	Log::Notice("Notice");
 	Log::Info("Info");
 	Log::Debug("Debug");
-
-	m_reactor.EventLoop();
 }
--- Code/App/App.h
+++ Code/App/App.h
@@ -1,12 +1,9 @@
 #pragma once
 
 #include "System/Log.h"
-#include "System/Reactor.h"
 
 class App
 {
-	Reactor m_reactor;
-
 public:
 	App() = default;
 
